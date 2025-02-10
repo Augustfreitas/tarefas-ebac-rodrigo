a1bd2a22f1141eba3588c67b91c79c3ff94f796d
Augustfreitas <81585523+Augustfreitas@users.noreply.github.com>
2025-02-10 09:56:21 -0300

    Add files via upload

---
+++ mod7/tarefa7/src/Cliente.java
@@ -0,0 +1,42 @@
+public class Cliente {
+    String nome;
+    int id;
+    double saldo;
+    String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public double setSaldo(double saldo) {
+       return this.saldo = saldo;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /*
+     * Metodo para relizar a conta do valor do item comprado e
+     * atualizar o novo saldo do cliente
+     */
+    public void fazerCompra(double valor){
+        if (saldo >= valor) {
+          setSaldo(saldo - valor);
+        }
+    }
+
+}
